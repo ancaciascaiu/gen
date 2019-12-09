95f6c2057e7a5d3731610005e765b2ea2b8bbaf0
Anca Ciascaiu <aciascaiu@cars.com>
2019-12-09 13:19:16 -0600

    TDD with generating a word map

--- lib/gen.ex
+++ lib/gen.ex
@@ -1,5 +1,29 @@
 defmodule Gen do
   def generate_new_text("lib/the_ghost_inside_my_house.txt" = text_file) do
-    text = File.read(text_file)
+    {:ok, text} = File.read(text_file)
+
+    text
+    |> generate_word_map()
+  end
+
+  defp generate_word_map(text) do
+    text
+    |> String.split([" ", ",", "-", ".", "\n", "\n\n"])
+    |> Stream.chunk_every(3, 1, :discard)
+    |> Enum.to_list()
+    |> add_or_update_pair()
+  end
+
+  defp add_or_update_pair(word_list) do
+    word_list
+    |> Enum.reduce(%{}, fn [word1, word2, word3], acc ->
+      case Map.fetch(acc, word1) do
+        :error ->
+          Map.put(acc, word1, [word2, word3])
+
+        {:ok, value} ->
+          Map.put(acc, word1, value ++ [word2, word3])
+      end
+    end)
   end
 end
--- test/gen_test.exs
+++ test/gen_test.exs
@@ -3,8 +3,9 @@ defmodule GenTest do
 
   test "generate_new_text/1" do
     text_file = "lib/the_ghost_inside_my_house.txt"
-    {:ok, text} = Gen.generate_new_text(text_file)
+    result = Gen.generate_new_text(text_file)
 
-    assert String.starts_with?(text, "The Ghosts Inside My House")
+    assert result["he’s"] == ["younger", "than"]
+    assert result["garden"] == ["while", "my", "for", "what", "we", "saw", "where", "the"]
   end
 end
